22381c1ba0b952b11f91742a51c21427208d1a92
Caesar(Xuanpei_Chen) <13122531903@163.com>
2025-08-02 21:55:59 +0800

    Update setup script to change working directory to /home/xuanpeichen before executing commands; correct requirements file name from 'requirements.txt' to 'requirement.txt'; add commands to enable SPI and set executable permissions for start.sh script.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 
 
-
+cd /home/xuanpeichen
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
 tar zxvf bcm2835-1.60.tar.gz 
 cd bcm2835-1.60/
@@ -18,11 +18,15 @@ git config --global user.name "Caesar723"
 git config --global user.email "13122531903@163.com"
 
 
+cd /home/xuanpeichen
+
 wget https://github.com/WiringPi/WiringPi/releases/download/2.61-1/wiringpi-2.61-1-arm64.deb
 sudo dpkg -i wiringpi-2.61-1-arm64.deb
 gpio -v
 rm -rf wiringpi-2.61-1-arm64.deb
 
+
+cd /home/xuanpeichen
 
 python3 -m venv myenv
 source ~/myenv/bin/activate
@@ -34,16 +38,22 @@ source ~/myenv/bin/activate
 sudo apt-get update
 sudo apt-get install -y libgtk-4-1 libgraphene-1.0-0
 
-pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+cd /home/xuanpeichen/Desktop/Ink-wash-photo-frame
+
+pip3 install -r requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install playwright -i https://pypi.tuna.tsinghua.edu.cn/simple
 playwright install
 
 
+sudo raspi-config nonint do_spi 0
 
 
 sudo systemctl daemon-reload
 sudo systemctl enable startscript.service
 sudo systemctl start startscript.service
+
+chmod +x /home/xuanpeichen/Desktop/Ink-wash-photo-frame/start.sh
+
 # pip3 install RPi.GPIO -i https://pypi.tuna.tsinghua.edu.cn/simple
 # pip3 install pillow numpy -i https://pypi.tuna.tsinghua.edu.cn/simple
 # pip3 install spidev -i https://pypi.tuna.tsinghua.edu.cn/simple
